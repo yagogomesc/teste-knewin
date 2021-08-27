fc1cab5569a55bab486ff1c669ad5baaca171847
Yago Gomes <yagogomesc@gmail.com>
2021-08-27 00:47:11 -0300

    Change form to simple file input

--- resources/views/notices/create.blade.php
+++ resources/views/notices/create.blade.php
@@ -5,14 +5,11 @@
 @section('content')
 
 <a href="/" class="btn btn-primary col-1 ml-5 mt-5">Voltar</a>
-<div class="card ml-5 mt-3 mr-5 p-3">
-    <form method="GET" action="/notices" enctype="multipart/form-data">
-        
-        <div class="mb-3">
-            <label for="formJson" class="form-label">Default file input example</label>
-            <input class="form-control" name="arquivo" type="file" accept="application/json" id="formJson">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+<section class="card ml-5 mt-3 mr-5 p-3">
+    <div class="mb-3">
+        <label for="fileJson" class="form-label">Selecione o arquivo</label>
+        <input class="form-control" name="arquivo" type="file" accept="application/json" id="fileJson">
+    </div>
+    <button type="submit" id="submitFile" class="btn btn-primary">Submit</button>
+</section>
 @endsection
